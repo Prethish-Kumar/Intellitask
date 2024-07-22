f9f2bf5692513d686e0b2371414de724971c7fd4
prethish kumar <prethishkumaroff@gmail.com>
2024-07-22 17:19:01 +0530

    Changed Shadcn Theme and Added Meta Data.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-"use client";
 import { api } from "@/convex/_generated/api";
 import { SignInButton } from "@clerk/nextjs";
 import { useMutation } from "convex/react";
 import Image from "next/image";
 import { useEffect } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "IntelliTask",
+  description: "Let AI systemize your tasks and simplify your life!",
+};
 
 export default function Home() {
   return (
